32a8dddd934b8459393b981c6079eae2118a26c7
Guillaume Chevallereau <gchevallereau@free.fr>
2011-08-11 20:20:31 +0200

    Code clean

--- src/Geoscape/EndResearchState.h
+++ src/Geoscape/EndResearchState.h
@@ -4,23 +4,20 @@
 #include "../Engine/State.h"
 namespace OpenXcom
 {
-  class Game;
-  class Window;
-  class TextButton;
-  class Text;
+class Game;
+class Window;
+class TextButton;
+class Text;
 
-  class EndResearchState : public State
-  {
-	TextButton *_btnOk, *_btnOk5Secs;
+class EndResearchState : public State
+{
 	Window *_window;
 	Text *_txtTitle;
-    // 	TextButton *_btnReport, *_btnOk;
-    // 	Window *_window;
-    // Text * _txtEndResearch;
-  public:
-    EndResearchState(Game * game);
+     	TextButton *_btnReport, *_btnOk;
+public:
+	EndResearchState(Game * game);
 	void btnOkClick(Action *action);
 	void btnReportClick(Action *action);
-  };
+};
 }
 #endif
