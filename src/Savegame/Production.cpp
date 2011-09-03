e81ce383878409553274a0e5acac6a976df18d44
Guillaume Chevallereau <gchevallereau@free.fr>
2011-09-03 16:05:50 +0200

    Added Production class

---
+++ src/Savegame/Production.cpp
@@ -0,0 +1,65 @@
+#include "Production.h"
+#include "../Ruleset/RuleManufactureInfo.h"
+#include "../Ruleset/RuleItem.h"
+#include "Base.h"
+#include "SavedGame.h"
+#include "ItemContainer.h"
+
+namespace OpenXcom
+{
+Production::Production (RuleItem * item, int todo) : _item(item), _todo(todo), _timeSpent(0), _engineers(0)
+{
+}
+
+int Production::getNumberOfItemTodo () const
+{
+	return _todo;
+}
+
+void Production::setNumberOfItemTodo (int todo)
+{
+	_todo = todo;
+}
+
+int Production::getTimeSpent () const
+{
+	return _timeSpent;
+}
+
+void Production::setTimeSpent (int done)
+{
+	_timeSpent = done;
+}
+
+int Production::getAssignedEngineers()
+{
+	return _engineers;
+}
+
+void Production::setAssignedEngineers (int engineers)
+{
+	_engineers = engineers;
+}
+
+bool Production::step(Base * b, SavedGame * g)
+{
+	int done = getNumberOfItemDone ();
+	_timeSpent += _engineers;
+	if (done < getNumberOfItemDone ())
+	{
+		b->getItems ()->addItem(_item->getType (), 1);
+		g->setFunds(g->getFunds() - _item->getManufactureInfo()->getManufactureCost ());
+	}
+	if (getNumberOfItemDone () >= _todo)
+	{
+		return true;
+	}
+	return false;
+}
+
+int Production::getNumberOfItemDone () const
+{
+	return _timeSpent / _item->getManufactureInfo()->getManufactureTime ();
+}
+
+};
---
+++ src/Savegame/Production.h
@@ -0,0 +1,27 @@
+#ifndef OPENXCOM_PRODUCTION
+#define OPENXCOM_PRODUCTION
+namespace OpenXcom
+{
+class RuleItem;
+class Base;
+class SavedGame;
+class Production
+{
+public:
+	Production (RuleItem * item, int todo);
+	int getNumberOfItemTodo () const;
+	void setNumberOfItemTodo (int);
+	int getTimeSpent () const;
+	void setTimeSpent (int);
+	int getNumberOfItemDone () const;
+	int getAssignedEngineers();
+	void setAssignedEngineers (int);
+	bool step(Base * b, SavedGame * g);
+private:
+	RuleItem * _item;
+	int _todo;
+	int _timeSpent;
+	int _engineers;
+};
+}
+#endif
