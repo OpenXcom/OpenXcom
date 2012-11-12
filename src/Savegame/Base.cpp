cb9c04e98b2545997e4ac624bc4e920f36318509
Warboy <neo_nihilist@hotmail.com>
2012-11-13 09:48:22 +1100

    Base Functions - function to return the total amount of alien containment at a given base - method in the debriefing screen for sending live aliens to storage, or displaying the "no containment" state if none found. - extra condition to exclude live aliens from the sell screen. - checks for containment at destination when transferring live aliens.

--- src/Savegame/Base.cpp
+++ src/Savegame/Base.cpp
@@ -943,4 +943,23 @@ bool Base::getHyperDetection() const
 	}
 	return false;
 }
+
+/**
+ * Returns the total amount of Containment Space
+ * available in the base.
+ * @return Containment Lab space.
+ */
+int Base::getAvailableContainment() const
+{
+	int total = 0;
+	for (std::vector<BaseFacility*>::const_iterator i = _facilities.begin(); i != _facilities.end(); ++i)
+	{
+		if ((*i)->getBuildTime() == 0)
+		{
+			total += (*i)->getRules()->getAliens();
+		}
+	}
+	return total;
+}
+
 }
