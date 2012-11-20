fbedd9e291484fe0a0d0c987fcc7973009fd207a
Konstantinos Arvanitis <konstantinos.arvanitis@gmail.com>
2012-11-20 10:51:59 +0200

    New localization code.
    We are now returning LocalizedText instead of plain std::wstring. LocalizedText
    understands how to replace markers with arguments, although no one is using it
    yet.

--- src/Engine/Language.cpp
+++ src/Engine/Language.cpp
@@ -19,10 +19,12 @@
 #include "Language.h"
 #include <locale>
 #include <fstream>
+#include <cassert>
 #include "CrossPlatform.h"
 #include "Logger.h"
 #include "Exception.h"
 #include "Options.h"
+#include "LocalizedText.h"
 #include "../Interface/TextList.h"
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
@@ -33,9 +35,194 @@ namespace OpenXcom
 {
 
 /**
+ * This class is the interface used to find plural forms for the different languages.
+ * Derived classes implement getKeys() according to the specific language's rules.
+ */
+class Language::PluralityRules
+{
+public:
+	/// Allow proper destruction through base pointer.
+	virtual ~PluralityRules() { /* Empty by design. */ }
+	/// Get dictionary key suffix for value of @a n.
+	/**
+	  @param count The number controlling the plurality.
+	  @return Pointer to the zero-terminated suffix string.
+	 */
+	virtual const char *getSuffix(unsigned n) const = 0;
+	/// Create a concrete instance for a given language.
+	static PluralityRules *create(const std::string &language);
+
+protected:
+	PluralityRules() { /* Empty by design. */ }
+private:
+	typedef PluralityRules *(*PFCreate)();
+	static std::map<std::string, PFCreate> s_factoryFunctions;
+};
+}
+
+namespace {
+using OpenXcom::Language;
+/**
+ * Plurality rules for English (also used as default).
+ * Provide rules for languages that have singular and plural, with zero using the plural form.
+ * @langsuffixes _1 for singular and _2 for plural.
+ */
+class ManyOneMany: public Language::PluralityRules
+{
+public:
+	virtual const char *getSuffix(unsigned n) const;
+	static PluralityRules *create() { return new ManyOneMany; }
+};
+
+const char *ManyOneMany::getSuffix(unsigned n) const
+{
+	if (n == 1)
+		return "_1";
+	return "_2";
+}
+
+/**
+ * Plurality rules for French.
+ * Provide rules for languages that have singular and plural, with zero using the singular form.
+ * @langsuffixes _1 for singular and _2 for plural.
+ */
+class FrenchRules: public Language::PluralityRules
+{
+public:
+	virtual const char *getSuffix(unsigned n) const;
+	static PluralityRules *create() { return new FrenchRules; }
+};
+
+const char *FrenchRules::getSuffix(unsigned n) const
+{
+	if (n < 2)
+		return "_1";
+	return "_2";
+}
+
+/**
+ * Plurality rules for Czech.
+ * @langsuffixes _1 n%100 == 1, 2<= n%100 <= 4, _3 otherwise.
+ */
+class CzechRules: public Language::PluralityRules
+{
+public:
+	virtual const char *getSuffix(unsigned n) const;
+	static PluralityRules *create() { return new CzechRules; }
+};
+
+const char *CzechRules::getSuffix(unsigned n) const
+{
+	if (n%100 == 1)
+		return "_1";
+	if (n%100 >= 2 && n%100 <=4)
+		return "_2";
+	return "_3";
+}
+
+/**
+ * Plurality rules for Polish.
+ * @langsuffixes _1 n%100 == 1, 2<= n%100 <= 4 && (n%100<10 || n%100>20), _3 otherwise.
+ */
+class PolishRules: public Language::PluralityRules
+{
+public:
+	virtual const char *getSuffix(unsigned n) const;
+	static PluralityRules *create() { return new PolishRules; }
+};
+
+const char *PolishRules::getSuffix(unsigned n) const
+{
+	if (n%100 == 1)
+		return "_1";
+	if ( 2 <= n%10 && n%10 <=4 && (n%100 < 10 || n%100 > 20))
+		return "_2";
+	return "_3";
+}
+
+/**
+ * Plurality rules for Romanian.
+ * @langsuffixes _1 n%100 == 1, _2 n == 0 || 1 <= n%100 <= 20, _3 otherwise.
+ */
+class RomanianRules: public Language::PluralityRules
+{
+public:
+	virtual const char *getSuffix(unsigned n) const;
+	static PluralityRules *create() { return new RomanianRules; }
+};
+
+const char *RomanianRules::getSuffix(unsigned n) const
+{
+	if (n%100 == 1)
+		return "_1";
+	if (0 == n || (1 <= n%100 && n%100 <= 20))
+		return "_2";
+	return "_3";
+}
+
+/**
+ * Plurality rules for Russian.
+ * @langsuffixes _1 n%10 == 1 && n%100 != 11, 2<= n%100 <= 4 && (n%100<10 || n%100>20), _3 otherwise.
+ */
+class RusianRules: public Language::PluralityRules
+{
+public:
+	virtual const char *getSuffix(unsigned n) const;
+	static PluralityRules *create() { return new RusianRules; }
+};
+
+const char *RusianRules::getSuffix(unsigned n) const
+{
+	if (n%10 == 1 && n%100 != 11)
+		return "_1";
+	if ( 2 <= n%10 && n%10 <=4 && (n%100 < 10 || n%100 > 20))
+		return "_2";
+	return "_3";
+}
+
+}
+
+namespace OpenXcom {
+
+/** A mapping of language to plurality rules.
+ * It is populated the first time plurality rules are requested.
+ * @see Language::PluralityRules::create
+ */
+std::map<std::string, Language::PluralityRules::PFCreate> Language::PluralityRules::s_factoryFunctions;
+
+/**
+ * Search and create a handler for the plurality rules of @a language.
+ * If the language was not found, a default with the same rules as English is returned.
+ * @param language The target language.
+ * @return A newly created PluralityRules instance for the given language.
+ * @todo Make sure the rest of the languages we support in OpenXcom are ok with
+ * the English rules for plurality.
+ * @internal The first time this is called, we populate the language => rules mapping.
+ */
+Language::PluralityRules *Language::PluralityRules::create(const std::string &language)
+{
+	// Populate factory the first time we are called.
+	if (s_factoryFunctions.empty())
+	{
+		s_factoryFunctions.insert(std::make_pair("FRANÇAIS", &FrenchRules::create));
+		s_factoryFunctions.insert(std::make_pair("ČESKY", &CzechRules::create));
+		s_factoryFunctions.insert(std::make_pair("POLSKI", &PolishRules::create));
+		s_factoryFunctions.insert(std::make_pair("ROMÂNĂ", &RomanianRules::create));
+		s_factoryFunctions.insert(std::make_pair("РУССКИЙ", &RusianRules::create));
+	}
+	PFCreate creator = &ManyOneMany::create;
+	std::map<std::string, PFCreate>::const_iterator found = s_factoryFunctions.find(language);
+	if (found != s_factoryFunctions.end())
+	{
+		creator = found->second;
+	}
+	return (*creator)();
+}
+
+/**
  * Initializes an empty language file.
  */
-Language::Language() : _name(L""), _strings()
+Language::Language() : _name(L""), _strings(), _handler(0)
 {
 
 }
@@ -45,10 +232,8 @@ Language::Language() : _name(L""), _strings()
  */
 Language::~Language()
 {
-
+	delete _handler;
 }
-
-
 
 /**
  * Takes a wide-character string and converts it
@@ -216,6 +401,20 @@ void Language::replace(std::string &str, const std::string &find, const std::str
 }
 
 /**
+ * Replaces every instance of a substring.
+ * @param str The string to modify.
+ * @param find The substring to find.
+ * @param replace The substring to replace it with.
+ */
+void Language::replace(std::wstring &str, const std::wstring &find, const std::wstring &replace)
+{
+	for (size_t i = str.find(find); i != std::wstring::npos; i = str.find(find, i + 1))
+	{
+		str.replace(i, find.length(), replace);
+	}
+}
+
+/**
  * Gets all the languages found in the
  * data folder and adds them to a text list.
  * @param list Text list.
@@ -272,7 +471,7 @@ std::vector<std::string> Language::getList(TextList *list)
  * in a text file into the Language. Each pair is made of
  * an ID and a localized string.
  * @param filename Filename of the LNG file.
- * @sa <a href="../language_id.html">Reference Table</a>
+ * @sa @ref LanguageFiles
  */
 void Language::loadLng(const std::string &filename)
 {
@@ -285,7 +484,7 @@ void Language::loadLng(const std::string &filename)
 	}
 
 	char value;
-	std::string buffer, bufid;
+	std::string buffer, bufid, language;
 	std::wstring bufstr;
 	bool first = true, id = true;
 
@@ -300,6 +499,7 @@ void Language::loadLng(const std::string &filename)
 			// Get language name
 			if (first)
 			{
+				language = buffer;
 				_name = utf8ToWstr(buffer);
 				first = false;
 			}
@@ -315,8 +515,9 @@ void Language::loadLng(const std::string &filename)
 				{
 					replace(buffer, "{NEWLINE}", "\n");
 					replace(buffer, "{SMALLLINE}", "\x02");
+					replace(buffer, "{ALT}", "\x01");
 					bufstr = utf8ToWstr(buffer);
-					_strings[bufid] = bufstr;
+					_strings[bufid] = LocalizedText(bufstr);
 				}
 				id = !id;
 			}
@@ -330,6 +531,8 @@ void Language::loadLng(const std::string &filename)
 	}
 
 	txtFile.close();
+	delete _handler;
+	_handler = PluralityRules::create(language);
 }
 
 /**
@@ -342,20 +545,21 @@ std::wstring Language::getName() const
 }
 
 /**
- * Returns the localizable string with the specified ID.
+ * Returns the localized text with the specified ID.
  * If it's not found, just returns the ID.
  * @param id ID of the string.
  * @return String with the requested ID.
  */
-std::wstring Language::getString(const std::string &id) const
+const LocalizedText &Language::getString(const std::string &id) const
 {
-	if (id.empty())
-		return L"";
-	std::map<std::string, std::wstring>::const_iterator s = _strings.find(id);
+	static LocalizedText hack(L"");
+	assert(!id.empty());
+	std::map<std::string, LocalizedText>::const_iterator s = _strings.find(id);
 	if (s == _strings.end())
 	{
 		Log(LOG_WARNING) << id << " not found in " << Options::getString("language");
-		return utf8ToWstr(id);
+		hack = LocalizedText(utf8ToWstr(id));
+		return hack;
 	}
 	else
 	{
@@ -364,34 +568,100 @@ std::wstring Language::getString(const std::string &id) const
 }
 
 /**
- * Outputs all the language IDs and strings
- * to an HTML table.
- * @param filename HTML file.
+ * Returns the localized text with the specified ID, in the proper form for @a n.
+ * The substitution of @a n has already happened in the returned LocalizedText.
+ * If it's not found, just returns the ID.
+ * @param id ID of the string.
+ * @param n Number to use to decide the proper form.
+ * @return String with the requested ID.
  */
-void Language::toHtml(const std::string &filename) const
+LocalizedText Language::getString(const std::string &id, unsigned n) const
 {
-	std::ofstream htmlFile (filename.c_str(), std::ios::out);
-	htmlFile << "<table border=\"1\" width=\"100%\">" << std::endl;
-	htmlFile << "<tr><th>ID String</th><th>English String</th></tr>" << std::endl;
-	for (std::map<std::string, std::wstring>::const_iterator i = _strings.begin(); i != _strings.end(); ++i)
+	assert(!id.empty());
+	std::map<std::string, LocalizedText>::const_iterator s = _strings.end();
+	if (0 == n)
 	{
-		htmlFile << "<tr><td>" << i->first << "</td><td>";
-		std::string s = wstrToUtf8(i->second);
-		for (std::string::const_iterator j = s.begin(); j != s.end(); ++j)
-		{
-			if (*j == 2 || *j == '\n')
-			{
-				htmlFile << "<br />";
-			}
-			else
-			{
-				htmlFile << *j;
-			}
-		}
-		htmlFile << "</td></tr>" << std::endl;
+		// Try specialized form.
+		s = _strings.find(id + "_0");
 	}
-	htmlFile << "</table>" << std::endl;
-	htmlFile.close();
+	if (s == _strings.end())
+	{
+		// Try proper form by language
+		s = _strings.find(id + _handler->getSuffix(n));
+	}
+	if (s == _strings.end())
+	{
+		Log(LOG_WARNING) << id << " not found in " << Options::getString("language");
+		return LocalizedText(utf8ToWstr(id));
+	}
+	std::wstringstream ss;
+	ss << n;
+	std::wstring marker(L"{N}"), val(ss.str()), txt(s->second);
+	replace(txt, marker, val);
+	return txt;
 }
 
 }
+
+/** @page LanguageFiles Format of the language files.
+
+Language files (.lng) contain UTF-8 text.
+The first line in a language file is the language's name, in the language itself.
+The rest of the file is processed in line pairs. The first line of each pair
+contains the ID string (dictionary key), the next line contains the localized
+text for the given key.
+
+The localized text may contain the following special markers:
+<table>
+<tr>
+ <td><tt>{</tt><i>1, 2, 3, ...</i> <tt>}</tt></td>
+ <td>These markers will be replaced by programmer-supplied values before the
+ message is displayed.</td></tr>
+<tr>
+ <td><tt>{ALT}</tt></td>
+ <td>The rest of the text will be in an alternate color. Using this again will
+ switch back to the primary color.</td></tr>
+<tr>
+ <td><tt>{NEWLINE}</tt></td>
+ <td>It will be replaced with a line break in the game.</td></tr>
+<tr>
+ <td>{SMALLLINE}</td>
+ <td>The rest of the text will be in a small font.</td></tr>
+</table>
+
+There is an additional marker sequence, that should only appear in texts that
+depend on a number. This marker <tt>{N}</tt> will be replaced by the actual
+number used. The keys for texts that depend on numbers also have special
+suffixes, that depend on the language. For all languages, a suffix of
+<tt>_0</tt> is tried if the number is zero, before trying the actual key
+according to the language rules. The rest of the suffixes depend on the language.
+
+<table>
+<caption>Current rules</caption>
+<tr><th>Language</th><th>Suffixes</th></tr>
+<tr><td>English</td><td><tt>_1</tt> n == 1, <tt>_2</tt> otherwise</td></tr>
+<tr><td>French</td><td><tt>_1</tt> n < 2, <tt>_2</tt> otherwise</td></tr>
+<tr><td>Czech</td><td><tt>_1</tt> n % 100 == 1, <tt>_2</tt> 2 <= n % 100 <= 4, <tt>_3</tt> otherwise</td></tr>
+<tr><td>Polish</td><td><tt>_1</tt> n % 100 == 1,
+ <tt>_2</tt> 2 <= n % 10 <= 4 && (n % 100 < 10 || n % 100 > 20),
+ <tt>_3</tt> otherwise</td></tr>
+<tr><td>Romanian</td><td><tt>_1</tt> n % 100 == 1,
+ <tt>_2</tt> n == 0 || 1 <= n % 100 <= 20,
+ <tt>_3</tt> otherwise</td></tr>
+<tr><td>Russian</td><td><tt>_1</tt> n % 10 == 1 && n % 100 != 11,
+ <tt>_2</tt> 2 <= n % 10 <= 4 && (n % 100 < 10 || n % 100 > 20),
+ <tt>_3</tt> otherwise</td></tr>
+<tr><td><i>Other languages</i></th><td><tt>_1</tt> n == 1, <tt>_2</tt> otherwise</td></tr>
+</table>
+
+So, you would write (for English):
+<pre>
+STR_ENEMIES_0
+There are no enemies left.
+STR_ENEMIES_1
+There is a single enemy left.
+STR_ENEMIES_2
+There are {N} enemies left.
+</pre>
+
+*/
---
+++ src/Engine/LocalizedText.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2010-2012 OpenXcom Developers.
+ *
+ * This file is part of OpenXcom.
+ *
+ * OpenXcom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenXcom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "LocalizedText.h"
+#include "Language.h"
+
+namespace OpenXcom
+{
+
+/**
+ * Replace the next argument placeholder with @a val.
+ * @param val The value to place in the next placeholder's position.
+ * @return A translated string with all occurences of the marker replaced by @a val.
+ */
+LocalizedText LocalizedText::arg(std::wstring const &val) const
+{
+	std::wostringstream os;
+	os << '{' << _nextArg << '}';
+	std::wstring marker(os.str());
+	size_t pos = _text.find(marker);
+	if (std::wstring::npos == pos)
+		return *this;
+	std::wstring ntext(_text);
+	for (/*empty*/ ; std::wstring::npos != pos; pos = ntext.find(marker, pos + 1))
+	{
+		ntext.replace(pos, marker.length(), val);
+	}
+	return LocalizedText(ntext, _nextArg);
+}
+
+/**
+ * Replace the next argument placeholder with @a val.
+ * @param val The value to place in the next placeholder's position.
+ * @return The translated string with all occurences of the marker replaced by @a val.
+ */
+LocalizedText &LocalizedText::arg(std::wstring const &val)
+{
+	std::wostringstream os;
+	os << '{' << _nextArg << '}';
+	std::wstring marker(os.str());
+	size_t pos = _text.find(marker);
+	if (std::wstring::npos != pos)
+	{
+		for (/*empty*/ ; std::wstring::npos != pos; pos = _text.find(marker, pos + 1))
+		{
+			_text.replace(pos, marker.length(), val);
+		}
+		++_nextArg;
+	}
+	return *this;
+}
+
+/**
+ * Return the UTF-8 representation of this string.
+ * @return A UTF-8 string.
+ */
+std::string LocalizedText::asUTF8() const
+{
+	return Language::wstrToUtf8(_text);
+}
+
+}
