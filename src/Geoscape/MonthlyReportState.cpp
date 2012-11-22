c5f82218e38f482217ba066b9693e9b1388a9030
Warboy <neo_nihilist@hotmail.com>
2012-11-22 20:11:36 +1100

    fixes for some exceptions make a check to see if we should make the check before we make the check. also, as per karvanit's suggestion, the conversion to const strings was dropped.

--- src/Geoscape/MonthlyReportState.cpp
+++ src/Geoscape/MonthlyReportState.cpp
@@ -152,17 +152,17 @@ MonthlyReportState::MonthlyReportState(Game *game, bool psi) : State(game), _psi
 	switch (_generalSatisfaction)
 	{
 	case 0:
-		ss4 << _game->getLanguage()->getString("STR_YOU_HAVE_NOT_SUCCEEDED").c_str();
+		ss4 << _game->getLanguage()->getString("STR_YOU_HAVE_NOT_SUCCEEDED");
 		// game over man
 		break;
 	case 1:
-		ss4 << _game->getLanguage()->getString("STR_COUNCIL_IS_DISSATISFIED").c_str();
+		ss4 << _game->getLanguage()->getString("STR_COUNCIL_IS_DISSATISFIED");
 		break;
 	case 3:
-		ss4 << _game->getLanguage()->getString("STR_COUNCIL_IS_VERY_PLEASED").c_str();
+		ss4 << _game->getLanguage()->getString("STR_COUNCIL_IS_VERY_PLEASED");
 		break;
 	default:
-		ss4 << _game->getLanguage()->getString("STR_COUNCIL_IS_GENERALLY_SATISFIED").c_str();
+		ss4 << _game->getLanguage()->getString("STR_COUNCIL_IS_GENERALLY_SATISFIED");
 		break;
 	}
 	if(_happyList.size())
@@ -170,25 +170,30 @@ MonthlyReportState::MonthlyReportState(Game *game, bool psi) : State(game), _psi
 		ss4 << "\n\n";
 		for(std::vector<std::string>::iterator happy = _happyList.begin(); happy != _happyList.end(); ++happy)
 		{
-			ss4 << _game->getLanguage()->getString(*happy).c_str();
-
-			if(happy == _happyList.end()-2 && _happyList.size() > 1)
+			ss4 << _game->getLanguage()->getString(*happy);
+			if(_happyList.size() > 1)
 			{
-				ss4 << _game->getLanguage()->getString("STR_AND").c_str();
-			}
-			if(happy != _happyList.end() - 1 && happy != _happyList.end() - 2 && _happyList.size() > 3)
-			{
-				ss4 << ", ";
+				if(happy == _happyList.end()-2)
+				{
+					ss4 << _game->getLanguage()->getString("STR_AND");
+				}
+				if(_happyList.size() > 3)
+				{
+					if(happy != _happyList.end() - 1 && happy != _happyList.end() - 2)
+					{
+						ss4 << ", ";
+					}
+				}
 			}
 			if(happy == _happyList.end()-1)
 			{
 				if(_happyList.size() > 1)
 				{
-					ss4 << _game->getLanguage()->getString("STR_COUNTRIES_ARE_PARTICULARLY_HAPPY").c_str();	
+					ss4 << _game->getLanguage()->getString("STR_COUNTRIES_ARE_PARTICULARLY_HAPPY");	
 				}
 				else
 				{
-					ss4 << _game->getLanguage()->getString("STR_COUNTRY_IS_PARTICULARLY_PLEASED").c_str();
+					ss4 << _game->getLanguage()->getString("STR_COUNTRY_IS_PARTICULARLY_PLEASED");
 				}
 			}
 		}
@@ -198,25 +203,30 @@ MonthlyReportState::MonthlyReportState(Game *game, bool psi) : State(game), _psi
 		ss4 << "\n\n";
 		for(std::vector<std::string>::iterator sad = _sadList.begin(); sad != _sadList.end(); ++sad)
 		{
-			ss4 << _game->getLanguage()->getString(*sad).c_str();
-
-			if(sad == _sadList.end()-2 && _sadList.size() > 1)
+			ss4 << _game->getLanguage()->getString(*sad);
+			if(_sadList.size() > 1)
 			{
-				ss4 << _game->getLanguage()->getString("STR_AND").c_str();
-			}
-			if(sad != _sadList.end() - 1 && sad != _sadList.end() - 2 && _sadList.size() > 3)
-			{
-				ss4 << ", ";
+				if(sad == _sadList.end()-2)
+				{
+					ss4 << _game->getLanguage()->getString("STR_AND");
+				}
+				if(_sadList.size() > 3)
+				{
+					if(sad != _sadList.end() - 1 && sad != _sadList.end() - 2)
+					{
+						ss4 << ", ";
+					}
+				}
 			}
 			if(sad == _sadList.end()-1)
 			{
 				if(_sadList.size() > 1)
 				{
-					ss4 << _game->getLanguage()->getString("STR_COUNTRIES_ARE_UNHAPPY_WITH_YOUR_ABILITY").c_str();	
+					ss4 << _game->getLanguage()->getString("STR_COUNTRIES_ARE_UNHAPPY_WITH_YOUR_ABILITY");	
 				}
 				else
 				{
-					ss4 << _game->getLanguage()->getString("STR_COUNTRY_IS_UNHAPPY_WITH_YOUR_ABILITY").c_str();
+					ss4 << _game->getLanguage()->getString("STR_COUNTRY_IS_UNHAPPY_WITH_YOUR_ABILITY");
 				}
 			}
 		}
@@ -226,25 +236,30 @@ MonthlyReportState::MonthlyReportState(Game *game, bool psi) : State(game), _psi
 		ss4 << "\n\n";
 		for(std::vector<std::string>::iterator pact = _pactList.begin(); pact != _pactList.end(); ++pact)
 		{
-			ss4 << _game->getLanguage()->getString(*pact).c_str();
-
-			if(pact == _pactList.end()-2 && _pactList.size() > 1)
+			ss4 << _game->getLanguage()->getString(*pact);
+			if(_pactList.size() > 1)
 			{
-				ss4 << _game->getLanguage()->getString("STR_AND").c_str();
-			}
-			if( pact != _pactList.end()-1 && pact != _pactList.end()-2 && _pactList.size() > 3)
-			{
-				ss4 << ", ";
+				if(pact == _pactList.end()-2)
+				{
+					ss4 << _game->getLanguage()->getString("STR_AND");
+				}
+				if(_pactList.size() > 3)
+				{
+					if(pact != _pactList.end() - 1 && pact != _pactList.end() - 2)
+					{
+						ss4 << ", ";
+					}
+				}
 			}
 			if(pact == _pactList.end()-1)
 			{
 				if(_pactList.size() > 1)
 				{
-					ss4 << _game->getLanguage()->getString("STR_COUNTRIES_HAVE_SIGNED_A_SECRET_PACT").c_str();	
+					ss4 << _game->getLanguage()->getString("STR_COUNTRIES_HAVE_SIGNED_A_SECRET_PACT");	
 				}
 				else
 				{
-					ss4 << _game->getLanguage()->getString("STR_COUNTRY_HAS_SIGNED_A_SECRET_PACT").c_str();
+					ss4 << _game->getLanguage()->getString("STR_COUNTRY_HAS_SIGNED_A_SECRET_PACT");
 				}
 			}
 		}
