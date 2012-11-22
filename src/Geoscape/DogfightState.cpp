90a0c77f44ccc92ebff6aaf72381cd947484917b
Warboy <neo_nihilist@hotmail.com>
2012-11-22 16:16:17 +1100

    Xcom Activity Points for Interceptions now that multiple interception is in, i can comfortably add points for shooting down UFOs, and penalties for losing an interceptor.
    with this, i believe all the scoring functions are in place.
    funding should change monthly, country's dispositions will be
    displayed properly. i'm not 100% sure i'm using the correct formulae
    for calculating overall happiness and monthly score.
    currently i'm averaging the the scores for all applicable regions/countries
    and using the inverse of the formula for unhappiness to represent happiness.
    
    the formula for pact signing is correct, as far as i can tell, but ufopedia
    doesn't list the conditions for anything other than pact signing, so i'm sure
    the rest is wrong (even though it DOES provide meaningful results)

--- src/Geoscape/DogfightState.cpp
+++ src/Geoscape/DogfightState.cpp
@@ -42,6 +42,10 @@
 #include "../Engine/Sound.h"
 #include "../Savegame/Base.h"
 #include "../Savegame/CraftWeaponProjectile.h"
+#include "../Savegame/Country.h"
+#include "../Ruleset/RuleCountry.h"
+#include "../Savegame/Region.h"
+#include "../Ruleset/RuleRegion.h"
 #include <cstdlib>
 
 namespace OpenXcom
@@ -922,6 +926,20 @@ void DogfightState::move()
 				{
 					if(*c == _craft)
 					{
+						for(std::vector<Country*>::iterator country = _game->getSavedGame()->getCountries()->begin(); country != _game->getSavedGame()->getCountries()->end(); ++country)
+						{
+							if((*country)->getRules()->insideCountry((*c)->getLongitude(), (*c)->getLatitude()));
+							{
+								(*country)->addActivityXcom(0-(*c)->getRules()->getScore());
+							}
+						}
+						for(std::vector<Region*>::iterator region = _game->getSavedGame()->getRegions()->begin(); region != _game->getSavedGame()->getRegions()->end(); ++region)
+						{
+							if((*region)->getRules()->insideRegion((*c)->getLongitude(), (*c)->getLatitude()));
+							{
+								(*region)->addActivityXcom(0-(*c)->getRules()->getScore());
+							}
+						}
 						delete *c;
 						(*b)->getCrafts()->erase(c);
 						break;
@@ -958,6 +976,20 @@ void DogfightState::move()
 		{
 			if(_ufo->getShotDownByCraftId() == _craft->getId())
 			{
+				for(std::vector<Country*>::iterator country = _game->getSavedGame()->getCountries()->begin(); country != _game->getSavedGame()->getCountries()->end(); ++country)
+				{
+					if((*country)->getRules()->insideCountry(_ufo->getLongitude(), _ufo->getLatitude()));
+					{
+						(*country)->addActivityXcom(_ufo->getRules()->getScore()*2);
+					}
+				}
+				for(std::vector<Region*>::iterator region = _game->getSavedGame()->getRegions()->begin(); region != _game->getSavedGame()->getRegions()->end(); ++region)
+				{
+					if((*region)->getRules()->insideRegion(_ufo->getLongitude(), _ufo->getLatitude()));
+					{
+						(*region)->addActivityXcom(_ufo->getRules()->getScore()*2);
+					}
+				}
 				setStatus("STR_UFO_DESTROYED");
 				_game->getResourcePack()->getSoundSet("GEO.CAT")->getSound(10)->play(); //11
 			}
@@ -969,6 +1001,20 @@ void DogfightState::move()
 			{
 				setStatus("STR_UFO_CRASH_LANDS");
 				_game->getResourcePack()->getSoundSet("GEO.CAT")->getSound(10)->play(); //10
+				for(std::vector<Country*>::iterator country = _game->getSavedGame()->getCountries()->begin(); country != _game->getSavedGame()->getCountries()->end(); ++country)
+				{
+					if((*country)->getRules()->insideCountry(_ufo->getLongitude(), _ufo->getLatitude()));
+					{
+						(*country)->addActivityXcom(_ufo->getRules()->getScore());
+					}
+				}
+				for(std::vector<Region*>::iterator region = _game->getSavedGame()->getRegions()->begin(); region != _game->getSavedGame()->getRegions()->end(); ++region)
+				{
+					if((*region)->getRules()->insideRegion(_ufo->getLongitude(), _ufo->getLatitude()));
+					{
+						(*region)->addActivityXcom(_ufo->getRules()->getScore());
+					}
+				}
 			}
 			if (!_globe->insideLand(_ufo->getLongitude(), _ufo->getLatitude()))
 			{
