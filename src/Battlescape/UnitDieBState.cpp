fa6ab88d68db7630b0b85d42af8f7abd3d51c32c
tomvd <tomvdtom@gmail.com>
2013-01-24 10:37:04 +0100

    - Fix cyberdisc exploding too often

--- src/Battlescape/UnitDieBState.cpp
+++ src/Battlescape/UnitDieBState.cpp
@@ -137,8 +137,11 @@ void UnitDieBState::think()
 		if (_unit->getSpecialAbility() == SPECAB_EXPLODEONDEATH)
 		{
 			_unit->instaKill();
-			Position p = Position(_unit->getPosition().x * 16, _unit->getPosition().y * 16, _unit->getPosition().z * 24);
-			_parent->statePushNext(new ExplosionBState(_parent, p, 0, _unit, 0));
+			if (_damageType != DT_STUN && _damageType != DT_HE)
+			{
+				Position p = Position(_unit->getPosition().x * 16, _unit->getPosition().y * 16, _unit->getPosition().z * 24);
+				_parent->statePushNext(new ExplosionBState(_parent, p, 0, _unit, 0));
+			}
 		}
 	}
 
