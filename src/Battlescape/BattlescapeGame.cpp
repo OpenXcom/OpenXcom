78064cec27638186a76f8f9342a3171675a5d278
Warboy <neo_nihilist@hotmail.com>
2013-02-25 23:25:11 +1100

    Allow unit selection in debug play mode.

--- src/Battlescape/BattlescapeGame.cpp
+++ src/Battlescape/BattlescapeGame.cpp
@@ -1196,7 +1196,7 @@ void BattlescapeGame::primaryAction(const Position &pos)
 	{
 		_currentAction.actor = _save->getSelectedUnit();
 		BattleUnit *unit = _save->selectUnit(pos);
-		if (unit && unit != _save->getSelectedUnit() && unit->getVisible())
+		if (unit && unit != _save->getSelectedUnit() && (unit->getVisible() || _debugPlay))
 		{
 		//  -= select unit =-
 			if (unit->getFaction() == _save->getSide())
