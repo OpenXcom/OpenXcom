fdbc9d629862b8569cc515c4fcc6f62e6828a2f9
Warboy <neo_nihilist@hotmail.com>
2013-03-10 22:20:52 +1100

    fix for bad ammo calculations
    make the function return a bool as to whether the ammo can be used or not,
    and don't fire a missile if we can't.

--- src/Geoscape/DogfightState.cpp
+++ src/Geoscape/DogfightState.cpp
@@ -1112,18 +1112,20 @@ void DogfightState::fireWeapon1()
 	if(_weapon1Enabled)
 	{
 		CraftWeapon *w1 = _craft->getWeapons()->at(0);
-		w1->setAmmo(w1->getAmmo() - 1);
+		if (w1->setAmmo(w1->getAmmo() - 1))
+		{
 
-		std::wstringstream ss;
-		ss << w1->getAmmo();
-		_txtAmmo1->setText(ss.str());
+			std::wstringstream ss;
+			ss << w1->getAmmo();
+			_txtAmmo1->setText(ss.str());
 
-		CraftWeaponProjectile *p = w1->fire();
-		p->setDirection(D_UP);
-		p->setHorizontalPosition(HP_LEFT);
-		_projectiles.push_back(p);
+			CraftWeaponProjectile *p = w1->fire();
+			p->setDirection(D_UP);
+			p->setHorizontalPosition(HP_LEFT);
+			_projectiles.push_back(p);
 
-		_game->getResourcePack()->getSound("GEO.CAT", w1->getRules()->getSound())->play();
+			_game->getResourcePack()->getSound("GEO.CAT", w1->getRules()->getSound())->play();
+		}
 	}
 }
 
@@ -1136,18 +1138,20 @@ void DogfightState::fireWeapon2()
 	if(_weapon2Enabled)
 	{
 		CraftWeapon *w2 = _craft->getWeapons()->at(1);
-		w2->setAmmo(w2->getAmmo() - 1);
+		if (w2->setAmmo(w2->getAmmo() - 1))
+		{
 
-		std::wstringstream ss;
-		ss << w2->getAmmo();
-		_txtAmmo2->setText(ss.str());
+			std::wstringstream ss;
+			ss << w2->getAmmo();
+			_txtAmmo2->setText(ss.str());
 
-		CraftWeaponProjectile *p = w2->fire();
-		p->setDirection(D_UP);
-		p->setHorizontalPosition(HP_RIGHT);
-		_projectiles.push_back(p);
+			CraftWeaponProjectile *p = w2->fire();
+			p->setDirection(D_UP);
+			p->setHorizontalPosition(HP_RIGHT);
+			_projectiles.push_back(p);
 
-		_game->getResourcePack()->getSound("GEO.CAT", w2->getRules()->getSound())->play();
+			_game->getResourcePack()->getSound("GEO.CAT", w2->getRules()->getSound())->play();
+		}
 	}
 }
 
