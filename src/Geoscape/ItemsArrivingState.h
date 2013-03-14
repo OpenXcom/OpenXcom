bc28d4e5035fc31ccbfc37c6514e46daabe57ad0
Warboy <neo_nihilist@hotmail.com>
2013-03-14 22:35:31 +1100

    fix #ifndef for those two

--- src/Geoscape/ItemsArrivingState.h
+++ src/Geoscape/ItemsArrivingState.h
@@ -16,8 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef OPENXCOM_TRANSFERSSTATE_H
-#define OPENXCOM_TRANSFERSSTATE_H
+#ifndef OPENXCOM_ITEMSARRIVINGSTATE_H
+#define OPENXCOM_ITEMSARRIVINGSTATE_H
 
 #include "../Engine/State.h"
 
