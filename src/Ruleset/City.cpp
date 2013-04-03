d08e1913703dba0bf3977a7f69634c84314a5079
Warboy <neo_nihilist@hotmail.com>
2013-04-04 04:07:13 +1100

    use degrees in the ruleset, convert to radians later.
    should take care of any gaps between region borders, and makes the data
    look prettier.

--- src/Ruleset/City.cpp
+++ src/Ruleset/City.cpp
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
+#define _USE_MATH_DEFINES
 #include "City.h"
 
 namespace OpenXcom
@@ -55,10 +56,12 @@ void City::load(const YAML::Node &node)
 		else if (key == "lon")
 		{
 			i.second() >> _lon;
+			_lon *= M_PI / 180;
 		}
 		else if (key == "lat")
 		{
 			i.second() >> _lat;
+			_lat *= M_PI / 180;
 		}
 	}
 }
