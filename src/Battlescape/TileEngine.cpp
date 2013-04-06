7c3217571cf0374b4aff2be68278515d58df3cbe
Warboy <neo_nihilist@hotmail.com>
2013-04-07 06:02:26 +1000

    the other half of the door fix
    fixed north east, but not south west. silly me.

--- src/Battlescape/TileEngine.cpp
+++ src/Battlescape/TileEngine.cpp
@@ -1728,6 +1728,33 @@ int TileEngine::unitOpensDoor(BattleUnit *unit, bool rClick)
 					if (tile) tile->openDoor(MapData::O_NORTHWALL);
 				}
 			}
+			if (unit->getDirection() == 5 && door == -1)
+			{
+				tile = _save->getTile(unit->getPosition() + Position(x,y,0) + Position(0, 1, 0));
+				if (tile)
+				{
+					door = tile->openDoor(MapData::O_WESTWALL, unit, _save->getDebugMode());
+				}
+				else
+				{
+					tile = _save->getTile(unit->getPosition() + Position(x,y,0));
+				}
+				if (door == 0 && rClick)
+					TUCost = tile->getTUCost(MapData::O_NORTHWALL, unit->getArmor()->getMovementType());
+				if (door == 1)
+				{
+					TUCost = tile->getTUCost(MapData::O_WESTWALL, unit->getArmor()->getMovementType());
+					// check for adjacent door(s)
+					tile = _save->getTile(unit->getPosition() + Position(x,y,0) + Position(1, 0, 0));
+					if (tile) tile->openDoor(MapData::O_WESTWALL);
+					tile = _save->getTile(unit->getPosition() + Position(x,y,0) + Position(-1, 0, 0));
+					if (tile) tile->openDoor(MapData::O_WESTWALL);
+					tile = _save->getTile(unit->getPosition() + Position(x,y,0) + Position(2, 0, 0));
+					if (tile) tile->openDoor(MapData::O_WESTWALL);
+					tile = _save->getTile(unit->getPosition() + Position(x,y,0) + Position(-2, 0, 0));
+					if (tile) tile->openDoor(MapData::O_WESTWALL);
+				}
+			}
 		}
 	}
 
