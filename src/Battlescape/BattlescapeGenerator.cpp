29f5fbf11abbc0a07c200a8201d12605c06aa2dd
Warboy <neo_nihilist@hotmail.com>
2013-05-15 22:49:41 +1000

    cave to peer pressure
    make aliens turn toward the ramp based on difficulty.

--- src/Battlescape/BattlescapeGenerator.cpp
+++ src/Battlescape/BattlescapeGenerator.cpp
@@ -693,7 +693,7 @@ BattleUnit *BattlescapeGenerator::addAlien(Unit *rules, int alienRank, bool outs
 		unit->setRankInt(alienRank);
 		int dir = _save->getTileEngine()->faceWindow(node->getPosition());
 		Position craft = _game->getSavedGame()->getBattleGame()->getUnits()->at(0)->getPosition();
-		if (_save->getTileEngine()->distance(node->getPosition(), craft) <= 20)
+		if (_save->getTileEngine()->distance(node->getPosition(), craft) <= 20 && RNG::generate(0,100) < 20 * (int)(_game->getSavedGame()->getDifficulty()))
 			dir = unit->getDirectionTo(craft);
 		if (dir != -1)
 			unit->setDirection(dir);
