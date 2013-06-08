5a72e5267b3b241779d0e60a9763454408db4a99
Warboy <neo_nihilist@hotmail.com>
2013-06-08 17:14:13 +1000

    fix some reaction fire stuff.
    turns out we were using unit aggression in completely the wrong manner
    0 reserves MORE than 2.
    
    this will result in less alien soldiers reaction firing, and more commanders
    doing so.

--- src/Battlescape/BattlescapeGame.cpp
+++ src/Battlescape/BattlescapeGame.cpp
@@ -187,11 +187,14 @@ void BattlescapeGame::handleAI(BattleUnit *unit)
 	{
 		switch (unit->getAggression())
 		{
+		case 0:
+			_tuReserved = BA_AIMEDSHOT;
+			break;
 		case 1:
-			_tuReserved = BA_SNAPSHOT;
+			_tuReserved = BA_AUTOSHOT;
 			break;
 		case 2:
-			_tuReserved = BA_AUTOSHOT;
+			_tuReserved = BA_SNAPSHOT;
 		default:
 			break;
 		}
@@ -997,10 +1000,15 @@ bool BattlescapeGame::checkReservedTU(BattleUnit *bu, int tu, bool justChecking)
 
 	if (_save->getSide() != FACTION_PLAYER) // aliens reserve TUs as a percentage rather than just enough for a single action.
 	{
+		if (_save->getSide() == FACTION_NEUTRAL)
+		{
+			return tu < bu->getTimeUnits();
+		}
 		switch (effectiveTuReserved)
 		{
-		case BA_SNAPSHOT: return tu + (bu->getStats()->tu / 3) < bu->getTimeUnits(); break;
-		case BA_AUTOSHOT: return tu + (bu->getStats()->tu / 2) < bu->getTimeUnits(); break;
+		case BA_SNAPSHOT: return tu + (bu->getStats()->tu / 3) < bu->getTimeUnits(); break; // 33%
+		case BA_AUTOSHOT: return tu + ((bu->getStats()->tu / 5)*2) < bu->getTimeUnits(); break; // 40%
+		case BA_AIMEDSHOT: return tu + (bu->getStats()->tu / 2) < bu->getTimeUnits(); break; // 50%
 		default: return tu < bu->getTimeUnits(); break;
 		}
 	}
