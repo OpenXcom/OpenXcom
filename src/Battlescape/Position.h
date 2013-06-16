b53c6893c7e1b46edfc285c72e925e475e5648b9
Warboy <neo_nihilist@hotmail.com>
2013-06-16 21:38:32 +1000

    add divisibility to Position objects.

--- src/Battlescape/Position.h
+++ src/Battlescape/Position.h
@@ -51,6 +51,9 @@ public:
 	Position& operator*=(const Position& pos) { x*=pos.x; y*=pos.y; z*=pos.z; return *this; }
 	Position operator*(const int v) const { return Position(x * v, y * v, z * v); }
 	Position& operator*=(const int v) { x*=v; y*=v; z*=v; return *this; }
+	
+	Position operator/(const Position& pos) const { return Position(x / pos.x, y / pos.y, z / pos.z); }
+	Position& operator/=(const Position& pos) { x/=pos.x; y/=pos.y; z/=pos.z; return *this; }
 
     Position operator/(const int v) const { return Position(x / v, y / v, z / v); }
 
