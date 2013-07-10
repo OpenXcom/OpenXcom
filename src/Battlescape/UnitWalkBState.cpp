4bced44b4157e732c979c01b726a2d5ab7c8e944
Warboy <neo_nihilist@hotmail.com>
2013-07-11 08:19:33 +1000

    charge TUs before first step
    handily, that bool already existed.

--- src/Battlescape/UnitWalkBState.cpp
+++ src/Battlescape/UnitWalkBState.cpp
@@ -44,7 +44,7 @@ namespace OpenXcom
 /**
  * Sets up an UnitWalkBState.
  */
-UnitWalkBState::UnitWalkBState(BattlescapeGame *parent, BattleAction action, const Position finalFacing, const bool pathfindForFinalTurn) : BattleState(parent, action), _unit(0), _pf(0), _terrain(0), _falling(false), _finalFacing(finalFacing), _pathfindForFinalTurn(pathfindForFinalTurn), _numUnitsSpotted(0)
+UnitWalkBState::UnitWalkBState(BattlescapeGame *parent, BattleAction action, const Position finalFacing, const bool pathfindForFinalTurn) : BattleState(parent, action), _unit(0), _pf(0), _terrain(0), _falling(false), _beforeFirstStep(false), _finalFacing(finalFacing), _pathfindForFinalTurn(pathfindForFinalTurn), _numUnitsSpotted(0)
 {
 
 }
@@ -66,6 +66,12 @@ void UnitWalkBState::init()
 	_terrain = _parent->getTileEngine();
 	_target = _action.target;
 	if (_parent->getSave()->getTraceSetting()) { Log(LOG_INFO) << "Walking from: " << _unit->getPosition().x << "," << _unit->getPosition().y << "," << _unit->getPosition().z << "," << " to " << _target.x << "," << _target.y << "," << _target.z;}
+	int dir = _pf->getStartDirection();
+	if (dir != -1 && dir != _unit->getDirection())
+	{
+		_unit->lookAt(dir);
+		_beforeFirstStep = true;
+	}
 }
 
 void UnitWalkBState::think()
@@ -404,7 +410,8 @@ void UnitWalkBState::think()
 				if (_unit->spendEnergy(energy))
 				{
 					Tile *tileBelow = _parent->getSave()->getTile(_unit->getPosition() + Position(0,0,-1));
-					_unit->startWalking(dir, destination, tileBelow, onScreen);
+					_unit->startWalking(dir, destination, tileBelow, onScreen);					
+					_beforeFirstStep = false;
 				}
 			}
 			// make sure the unit sprites are up to date
@@ -430,7 +437,25 @@ void UnitWalkBState::think()
 	// turning during walking costs no tu
 	if (_unit->getStatus() == STATUS_TURNING)
 	{
-		_unit->turn();
+		// except before the first step.
+		if (_beforeFirstStep)
+		{
+			if (_unit->spendTimeUnits(1))
+			{
+				_unit->turn();
+			}
+			else
+			{
+				_action.result = "STR_NOT_ENOUGH_TIME_UNITS";
+				_pf->abortPath();
+				_parent->popState();
+				return;
+			}
+		}
+		else
+		{
+			_unit->turn();
+		}
 
 		// calculateFOV is unreliable for setting the unitSpotted bool, as it can be called from various other places
 		// in the code, ie: doors opening, and this messes up the result.
