285c8d66abc4d02aa08649f48e335c693adbc500
redv <rproskurin@yandex.ru>
2013-07-18 01:16:24 +0400

    checking brackets

--- src/Basescape/NewResearchListState.cpp
+++ src/Basescape/NewResearchListState.cpp
@@ -106,8 +106,7 @@ void NewResearchListState::onSelectProject(Action *)
 {
 	_game->pushState(new ResearchInfoState(_game, _base, _projects[_lstResearch->getSelectedRow()]));
 	RuleResearch *_proj = _projects[_lstResearch->getSelectedRow()];
-	if (_proj->needItem()
-		&& (_game->getRuleset()->getUnit(_proj->getName())) || Options::getBool("xcom2012ResearchRules"))
+	if (_proj->needItem() && (_game->getRuleset()->getUnit(_proj->getName()) || Options::getBool("xcom2012ResearchRules")))
 	{
 		_base->getItems()->removeItem(_proj->getName(), 1);
 	}
