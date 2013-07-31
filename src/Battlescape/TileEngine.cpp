9e557245b3fd2082862c2ccf62d06c77556c3e11
Warboy <neo_nihilist@hotmail.com>
2013-07-31 11:00:37 +1000

    fix MC auto end thing
    seriously, how is it that this option causes so much trouble?

--- src/Battlescape/TileEngine.cpp
+++ src/Battlescape/TileEngine.cpp
@@ -2453,7 +2453,7 @@ bool TileEngine::psiAttack(BattleAction *action)
 				_save->getBattleState()->getBattleGame()->tallyUnits(liveAliens, liveSoldiers, false);
 				if (liveAliens == 0 || liveSoldiers == 0)
 				{
-					_save->getBattleState()->getBattleGame()->statePushBack(0);
+					_save->getBattleState()->getBattleGame()->requestEndTurn();
 				}
 			}
 		}
