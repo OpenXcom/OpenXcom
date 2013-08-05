8df81c2d5ace9cf0282413999bc39d4d5606719a
Warboy <neo_nihilist@hotmail.com>
2013-08-05 13:36:23 +1000

    fix some warnings

--- src/Savegame/BattleUnit.cpp
+++ src/Savegame/BattleUnit.cpp
@@ -51,8 +51,8 @@ BattleUnit::BattleUnit(Soldier *soldier, UnitFaction faction) : _faction(faction
 																_verticalDirection(0), _status(STATUS_STANDING), _walkPhase(0), _fallPhase(0), _kneeled(false), _floating(false),
 																_dontReselect(false), _fire(0), _currentAIState(0), _visible(false), _cacheInvalid(true),
 																_expBravery(0), _expReactions(0), _expFiring(0), _expThrowing(0), _expPsiSkill(0), _expMelee(0),
-																_turretType(-1), _motionPoints(0), _kills(0), _geoscapeSoldier(soldier), _charging(0), _turnsExposed(255),
-																_unitRules(0), _rankInt(-1), _hitByFire(false), _hidingForTurn(false), _moraleRestored(0), _coverReserve(0)
+																_motionPoints(0), _kills(0), _hitByFire(false), _moraleRestored(0), _coverReserve(0), _charging(0),
+																_turnsExposed(255), _unitRules(0), _geoscapeSoldier(soldier), _rankInt(-1), _turretType(-1), _hidingForTurn(false)
 {
 	_name = soldier->getName();
 	_id = soldier->getId();
@@ -117,9 +117,10 @@ BattleUnit::BattleUnit(Unit *unit, UnitFaction faction, int id, Armor *armor, in
 																						_toDirectionTurret(0),  _verticalDirection(0), _status(STATUS_STANDING), _walkPhase(0),
 																						_fallPhase(0), _kneeled(false), _floating(false), _dontReselect(false), _fire(0), _currentAIState(0),
 																						_visible(false), _cacheInvalid(true), _expBravery(0), _expReactions(0), _expFiring(0),
-																						_expThrowing(0), _expPsiSkill(0), _expMelee(0), _turretType(-1), _motionPoints(0), _kills(0),
-																						_armor(armor), _geoscapeSoldier(0), _charging(0), _turnsExposed(0), _unitRules(unit), _rankInt(-1),
-																						_hitByFire(false), _hidingForTurn(false), _moraleRestored(0), _coverReserve(0)
+																						_expThrowing(0), _expPsiSkill(0), _expMelee(0), _motionPoints(0), _kills(0), _hitByFire(false),
+																						_moraleRestored(0), _coverReserve(0), _charging(0), _turnsExposed(255),
+																						_armor(armor), _geoscapeSoldier(0),  _unitRules(unit), _rankInt(-1),
+																						_turretType(-1), _hidingForTurn(false)
 {
 	_type = unit->getType();
 	_rank = unit->getRank();
