d21b3e104cc9888bbe20569f62bb7226d07ca17c
redv <rproskurin@yandex.ru>
2013-10-31 14:52:08 +0400

    don't need centered every hit!

--- src/Battlescape/ExplosionBState.cpp
+++ src/Battlescape/ExplosionBState.cpp
@@ -129,7 +129,8 @@ void ExplosionBState::init()
 		_parent->getMap()->getExplosions()->insert(explosion);
 		// bullet hit sound
 		_parent->getResourcePack()->getSound("BATTLE.CAT", _item->getRules()->getHitSound())->play();
-		_parent->getMap()->getCamera()->centerOnPosition(t->getPosition(), false);
+		if (hit && t->getVisible() && t->getUnit() && t->getUnit()->getFaction() != FACTION_HOSTILE)
+			_parent->getMap()->getCamera()->centerOnPosition(t->getPosition(), false);
 	}
 }
 
