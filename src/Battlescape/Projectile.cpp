58d09767b3e12430ff9dee1a4b66465ed60d837e
Warboy <neo_nihilist@hotmail.com>
2013-12-19 20:34:01 +1100

    if i hear one more god damn word about blaster launchers...
    i swear to god Alice, BANG, ZOOM! Straight to the moon!

--- src/Battlescape/Projectile.cpp
+++ src/Battlescape/Projectile.cpp
@@ -96,12 +96,12 @@ int Projectile::calculateTrajectory(double accuracy)
 			if (_action.target != _origin)
 			{
 				// launched missiles go slightly higher than the middle.
-				targetVoxel.z = 16;
+				targetVoxel.z += 4;
 			}
 			else
 			{
 				// unless two waypoints are placed on the same tile, in which case target the floor.
-				targetVoxel.z = 2;
+				targetVoxel.z -= 10;
 			}
 		}
 	}
