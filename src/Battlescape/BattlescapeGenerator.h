5e8c2ef1ca1c27bc2ba1124846318171be38b593
Warboy <neo_nihilist@hotmail.com>
2013-12-22 05:37:40 +1100

    haha whoops

--- src/Battlescape/BattlescapeGenerator.h
+++ src/Battlescape/BattlescapeGenerator.h
@@ -74,7 +74,7 @@ private:
 	/// Adds a civlian to the game.
 	BattleUnit *addCivilian(Unit *rules);
 	/// Places an item on a soldier based on equipment layout.
-	BattleItem* placeItemByLayout(BattleItem *item);
+	bool placeItemByLayout(BattleItem *item);
 	/// Adds an item to a unit and the game.
 	bool addItem(BattleItem *item, BattleUnit *unit, bool allowSecondClip = false);
 	/// Loads an XCom MAP file.
