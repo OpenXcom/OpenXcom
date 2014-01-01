a5cb4edb68ba6f9c6f5c7a3f136ff16942803d1a
fenyo1 <fenyo.pub+github@gmail.com>
2014-01-01 01:24:50 +0100

    PsiTrainingState is now scrollable. (if the player has more than 8 bases with PsiLabs)

--- src/Geoscape/PsiTrainingState.h
+++ src/Geoscape/PsiTrainingState.h
@@ -25,6 +25,7 @@ namespace OpenXcom
 {
 
 class TextButton;
+class ArrowButton;
 class Window;
 class Text;
 class Base;
@@ -38,17 +39,44 @@ private:
 	TextButton *_btnOk;
 	Window *_window;
 	Text *_txtTitle;
+	ArrowButton *_up, *_down;
+	Timer *_timerUp, *_timerDown;
 	std::vector<TextButton*> _btnBases;
 	std::vector<Base*> _bases;
+	size_t _startIndex;
+	/// Handler for clicking the OK button.
+	void btnOkClick(Action *action);
+	/// Handler for clicking a Base button.
+	void btnBaseXClick(Action *action);
+	/// Handler for pressing the up arrowbutton.
+	void upPress(Action * action);
+	/// Handler for releasing the up arrowbutton.
+	void upRelease(Action * action);
+	/// Handler for clicking the up arrowbutton.
+	void upClick(Action * action);
+	/// Handler for pressing the down arrowbutton.
+	void downPress(Action * action);
+	/// Handler for releasing the down arrowbutton.
+	void downRelease(Action * action);
+	/// Handler for clicking the down arrowbutton.
+	void downClick(Action * action);
+	/// Event handler for _timerUp.
+	void onTimerUp();
+	/// Event handler for _timerDown.
+	void onTimerDown();
+	/// Scrolls up by the number of change.
+	void scrollUp(int change);
+	/// Scrolls down by the number of change.
+	void scrollDown(int change);
+	/// Updates the names of the _baseButtons, and arrowButtons.
+	void updateButtons();
+	/// Runs state functionality every cycle.
+	void think();
 public:
 	/// Creates the Psi Training state.
 	PsiTrainingState(Game *game);
 	/// Cleans up the Psi Training state.
 	~PsiTrainingState();
-	/// Handler for clicking the OK button.
-	void btnOkClick(Action *action);
-	/// Handler for clicking a Base button.
-	void btnBaseXClick(Action *action);
 };
 
 }
