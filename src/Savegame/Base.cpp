c5cd993064411ebeb3edea52d327463a3c57ad76
Carlos Failde <carlosfailde@gmail.com>
2014-01-08 18:22:09 +0000

    Typo

--- src/Savegame/Base.cpp
+++ src/Savegame/Base.cpp
@@ -1382,7 +1382,7 @@ void Base::destroyFacility(std::vector<BaseFacility*>::iterator &facility)
 			}
 			for (std::vector<Craft*>::iterator i = _crafts.begin(); i != _crafts.end(); ++i)
 			{
-				if (*i = (*facility)->getCraft())
+				if (*i == (*facility)->getCraft())
 				{
 					delete (*i);
 					_crafts.erase(i);
