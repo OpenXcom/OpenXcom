304526458944ddc36d00e9f9608a76090324d93b
Warboy <neo_nihilist@hotmail.com>
2014-03-02 17:16:58 +1100

    fix base inventory vis-a-vis deployments

--- src/Battlescape/BattlescapeGenerator.cpp
+++ src/Battlescape/BattlescapeGenerator.cpp
@@ -601,7 +601,7 @@ BattleUnit *BattlescapeGenerator::addXCOMUnit(BattleUnit *unit)
 			}
 		}
 	}
-	else if (_craft && !_craft->getRules()->getDeployment().empty())
+	else if (_craft && !_craft->getRules()->getDeployment().empty() && _mapsize_y != 1) // _mapsize_y 1 means we're in the faked out base inventory
 	{
 		for (std::vector<std::vector<int> >::const_iterator i = _craft->getRules()->getDeployment().begin(); i != _craft->getRules()->getDeployment().end(); ++i)
 		{
