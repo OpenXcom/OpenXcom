0db348740b7c5f1386b1ee1e60d1ecdf72e1730f
SupSuper <supsuper@gmail.com>
2014-04-02 04:39:55 +0100

    Fixes

--- src/Interface/ComboBox.cpp
+++ src/Interface/ComboBox.cpp
@@ -28,9 +28,6 @@
 namespace OpenXcom
 {
 
-const int ComboBox::LIST_MARGIN;
-const int ComboBox::MAX_ITEMS;
-
 /**
  * Sets up a combobox with the specified size and position.
  * @param state Pointer to state the window belongs to.
