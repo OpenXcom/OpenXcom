b578a49ebd77081a8c15b8fa1a5c6646ea76acda
SupSuper <supsuper@gmail.com>
2014-04-11 17:41:20 +0100

    Code cleanup

--- src/Geoscape/PsiTrainingState.cpp
+++ src/Geoscape/PsiTrainingState.cpp
@@ -68,21 +68,23 @@ PsiTrainingState::PsiTrainingState(Game *game) : State(game)
 	_txtTitle->setBig();
 	_txtTitle->setAlign(ALIGN_CENTER);
 	_txtTitle->setText(tr("STR_PSIONIC_TRAINING"));
+
 	int buttons = 0;
-	TextButton *_btnBase;
 	for(std::vector<Base*>::const_iterator b = _game->getSavedGame()->getBases()->begin(); b != _game->getSavedGame()->getBases()->end(); ++b)
 	{
 		if((*b)->getAvailablePsiLabs())
 		{
+			TextButton *btnBase = new TextButton(160, 14, 80, 40 + 16 * buttons);
+			btnBase->setColor(Palette::blockOffset(15) + 6);
+			btnBase->onMouseClick((ActionHandler)&PsiTrainingState::btnBaseXClick);
+			btnBase->setText((*b)->getName());
+			add(btnBase);
 			_bases.push_back(*b);
-			if (buttons < 8)
+			_btnBases.push_back(btnBase);
+			++buttons;
+			if (buttons >= 8)
 			{
-				_btnBase = new TextButton(160, 14, 80, 40 + 16*buttons);
-				_btnBase->setColor(Palette::blockOffset(15)+6);
-				_btnBase->onMouseClick((ActionHandler)&PsiTrainingState::btnBaseXClick);
-				_btnBase->setText((*b)->getName());
-				add(_btnBase);
-				++buttons;
+				break;
 			}
 		}
 	}
@@ -117,8 +119,14 @@ void PsiTrainingState::btnOkClick(Action *)
 
 void PsiTrainingState::btnBaseXClick(Action *action)
 {
-	int buttonIndex = (action->getSender()->getY() - (40 + _game->getScreen()->getDY())) / 16;
-	_game->pushState (new AllocatePsiTrainingState(_game, _bases.at(buttonIndex)));
+	for (size_t i = 0; i < _btnBases.size(); ++i)
+	{
+		if (action->getSender() == _btnBases[i])
+		{
+			_game->pushState(new AllocatePsiTrainingState(_game, _bases.at(i)));
+			break;
+		}
+	}
 }
 
 }
