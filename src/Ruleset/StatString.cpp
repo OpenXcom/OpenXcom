5f40c13d029d4f2d48a90b9f40e435f21d5808f7
boozedog <code@boozedog.com>
2014-04-23 16:16:14 -0400

    Handle conditionNames array a little less crappily

--- src/Ruleset/StatString.cpp
+++ src/Ruleset/StatString.cpp
@@ -42,23 +42,22 @@ StatString::~StatString()
  */
 void StatString::load(const YAML::Node &node)
 {
-	// TODO: conditionNames should be stored elsewhere
-	std::string conditionNames[7] = {"psiStrength", "psiSkill", "bravery", "strength", "firing", "reactions", "stamina"};
+	// NOTE: conditionNames must be terminated with an empty string
+	std::string conditionNames[] = {"psiStrength", "psiSkill", "bravery", "strength", "firing", "reactions", "stamina", ""};
 	_string = node["string"].as<std::string>(_string);
-	int ii;
-	// TODO: we need count of conditionNames here
-	for (ii=0; ii < 7; ii++)
+	int i = 0;
+	while (conditionNames[i] != "")
 	{
-		if (node[conditionNames[ii]])
+		if (node[conditionNames[i]])
 		{
-			_conditions[conditionNames[ii]] = getCondition(conditionNames[ii], node);
+			_conditions[conditionNames[i]] = getCondition(conditionNames[i], node);
 		}
+		i++;
 	}
 }
 
 StatStringLimits *StatString::getCondition(const std::string &conditionName, const YAML::Node &node)
 {
-	// TODO: use an enum or something here
 	int minValue = -999999999, maxValue = 999999999;
 	if (node[conditionName][0]) {
 		minValue = node[conditionName][0].as<int>(minValue);
