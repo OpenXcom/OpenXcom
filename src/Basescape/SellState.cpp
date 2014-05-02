65559653df8c2c4a9583bf7959ca3ddbb227f3c1
Warboy <neo_nihilist@hotmail.com>
2014-05-02 13:32:17 +1000

    initialize variable outside of switch.

--- src/Basescape/SellState.cpp
+++ src/Basescape/SellState.cpp
@@ -562,6 +562,7 @@ void SellState::changeByValue(int change, int dir)
 	// Calculate the change in storage space in tenths of an XCom storage unit.
 	Craft *craft;
 	RuleItem *armor, *item, *weapon, *ammo;
+	double total = 0.0;
 	switch (getType(_sel))
 	{
 	case SELL_SOLDIER:
@@ -572,7 +573,6 @@ void SellState::changeByValue(int change, int dir)
 		}
 		break;
 	case SELL_CRAFT:
-		double total = 0.0;
 		craft = _crafts[getCraftIndex(_sel)];
 		for (std::vector<CraftWeapon*>::iterator w = craft->getWeapons()->begin(); w != craft->getWeapons()->end(); ++w)
 		{
