088fed798890cb3a7ba9404d1cb1c30d6484dee5
Warboy <neo_nihilist@hotmail.com>
2014-05-25 17:01:00 +1000

    switch scale for inventory/minimap/etc
    make em full screen
    
    i'm going to regret this in the morning, aren't i?

--- src/Battlescape/MiniMapState.cpp
+++ src/Battlescape/MiniMapState.cpp
@@ -41,6 +41,10 @@ namespace OpenXcom
  */
 MiniMapState::MiniMapState (Game * game, Camera * camera, SavedBattleGame * battleGame) : State(game)
 {
+	Options::baseXResolution = Screen::ORIGINAL_WIDTH;
+	Options::baseYResolution = Screen::ORIGINAL_HEIGHT;
+	_game->getScreen()->resetDisplay(false);
+
 	_surface = new InteractiveSurface(320, 200);
 	_miniMapView = new MiniMapView(221, 148, 48, 16, game, camera, battleGame);
 	InteractiveSurface * btnLvlUp = new InteractiveSurface(18, 20, 24, 62);
@@ -127,6 +131,8 @@ void MiniMapState::handle(Action *action)
  */
 void MiniMapState::btnOkClick(Action *)
 {
+	Screen::updateScale(Options::battlescapeScale, Options::battlescapeScale, Options::baseXBattlescape, Options::baseYBattlescape, true);
+	_game->getScreen()->resetDisplay(false);
 	_game->popState();
 }
 
