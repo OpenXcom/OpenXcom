3ebbda2bff93a6f9472359a25e5eede1cced9533
Warboy <neo_nihilist@hotmail.com>
2014-05-31 14:55:15 +1000

    fix geoscape borders

--- src/Geoscape/GeoscapeState.cpp
+++ src/Geoscape/GeoscapeState.cpp
@@ -438,6 +438,9 @@ GeoscapeState::~GeoscapeState()
 void GeoscapeState::blit()
 {
 	State::blit();
+	_game->getScreen()->getSurface()->drawLine(_btnTop->getX() - 1, 0, _btnTop->getX() - 1, _game->getScreen()->getSurface()->getHeight(), 15);
+	_game->getScreen()->getSurface()->drawLine(_btnTop->getX(), _sidebar->getY() - 1, _game->getScreen()->getSurface()->getWidth(), _sidebar->getY() - 1, 15);
+	_game->getScreen()->getSurface()->drawLine(_btnBottom->getX(), _btnBottom->getY() - 1, _game->getScreen()->getSurface()->getWidth(), _btnBottom->getY() - 1, 15);
 	for(std::list<DogfightState*>::iterator it = _dogfights.begin(); it != _dogfights.end(); ++it)
 	{
 		(*it)->blit();
