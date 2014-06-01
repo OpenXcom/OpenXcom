24328fadcb228048ea9b634c4c0e9d7c0b9e5dd8
Warboy <neo_nihilist@hotmail.com>
2014-06-02 00:20:18 +1000

    fix throw math
    (double)(int / int) vs (double / double)

--- src/Battlescape/TileEngine.cpp
+++ src/Battlescape/TileEngine.cpp
@@ -2680,7 +2680,7 @@ bool TileEngine::validateThrow(BattleAction &action, Position originVoxel, Posit
 	double curvature = 0.5;
 	if (action.type == BA_THROW)
 	{
-		curvature = std::max(0.48, 1.73 / sqrt(sqrt((double)(action.actor->getStats()->strength / action.weapon->getRules()->getWeight()))) + (action.actor->isKneeled()? 0.1 : 0.0));
+		curvature = std::max(0.48, 1.73 / sqrt(sqrt((double)(action.actor->getStats()->strength) / (double)(action.weapon->getRules()->getWeight()))) + (action.actor->isKneeled()? 0.1 : 0.0));
 	}
 	Tile *targetTile = _save->getTile(action.target);
 	// object blocking - can't throw here
