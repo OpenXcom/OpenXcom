1598fe6b8b958cde7b0fd1ca1faebb46bdf04d27
Warboy <neo_nihilist@hotmail.com>
2014-09-21 02:39:42 +1000

    no intercept on return voyage

--- src/Geoscape/InterceptState.cpp
+++ src/Geoscape/InterceptState.cpp
@@ -196,7 +196,7 @@ void InterceptState::btnGotoBaseClick(Action *)
 void InterceptState::lstCraftsLeftClick(Action *)
 {
 	Craft* c = _crafts[_lstCrafts->getSelectedRow()];
-	if (c->getStatus() == "STR_READY" || ((c->getStatus() == "STR_OUT" || Options::craftLaunchAlways) && !c->getLowFuel()))
+	if (c->getStatus() == "STR_READY" || ((c->getStatus() == "STR_OUT" || Options::craftLaunchAlways) && !c->getLowFuel() && !c->getMissionComplete()))
 	{
 		_game->popState();
 		if (_target == 0)
