81bc9df726ea99fce8394b42fec8b12476d3683c
Warboy <neo_nihilist@hotmail.com>
2014-11-24 08:30:54 +1100

    copy visibility flag instead of relying on faction
    because vanilla.

--- src/Battlescape/BattlescapeGame.cpp
+++ src/Battlescape/BattlescapeGame.cpp
@@ -1558,6 +1558,7 @@ void BattlescapeGame::dropItem(const Position &position, BattleItem *item, bool 
  */
 BattleUnit *BattlescapeGame::convertUnit(BattleUnit *unit, const std::string &newType)
 {
+	bool visible = unit->getVisible();
 	getSave()->getBattleState()->showPsiButton(false);
 	// in case the unit was unconscious
 	getSave()->removeUnconsciousBodyItem(unit);
@@ -1609,10 +1610,7 @@ BattleUnit *BattlescapeGame::convertUnit(BattleUnit *unit, const std::string &ne
 	getSave()->getItems()->push_back(bi);
 	getTileEngine()->calculateFOV(newUnit->getPosition());
 	getTileEngine()->applyGravity(newUnit->getTile());
-	if (unit->getFaction() == FACTION_PLAYER)
-	{
-		newUnit->setVisible(true);
-	}
+	newUnit->setVisible(visible);
 	//newUnit->getCurrentAIState()->think();
 	return newUnit;
 
