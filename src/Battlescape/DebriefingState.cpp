229c534163915bc01cddadd06290883cfb6e480d
Shoes01 <the_original_lovok@hotmail.com>
2015-05-26 21:50:49 -0300

    Moved best-of medal logic out of needless loop

--- src/Battlescape/DebriefingState.cpp
+++ src/Battlescape/DebriefingState.cpp
@@ -254,6 +254,41 @@ DebriefingState::DebriefingState() : _region(0), _country(0), _noContainment(fal
     _missionStatistics->daylight = save->getSavedBattle()->getGlobalShade();
     _missionStatistics->id = _game->getSavedGame()->getMissionStatistics()->size();
 
+	// Award Best-of commendations
+	std::map<SoldierRank, Soldier*> bestSoldierCandidates;
+	int bestScore[7] = {0, 0, 0, 0, 0, 0, 0};
+	int bestOverallScore = 0;
+	// Check to see if any of the dead soldiers were exceptional.
+	for (std::vector<Soldier*>::iterator deadUnit = _game->getSavedGame()->getDeadSoldiers()->begin(); deadUnit != _game->getSavedGame()->getDeadSoldiers()->end(); ++deadUnit)
+	{                
+        /// Best-of awards
+		// Find the best soldier per rank by comparing score
+		int score = (*deadUnit)->getDiary()->getScoreTotal();
+		SoldierRank rank = (*deadUnit)->getRank();		
+		// Rookies don't get this award. No one likes them.
+		//if (rank == 0) continue;
+				
+		if (score > bestScore[rank])
+		{
+			bestSoldierCandidates[rank] = (*deadUnit);
+			bestScore[rank] = score;
+			if (score > bestOverallScore)
+			{
+				bestOverallScore = score;
+			}
+		}				
+	}
+	// Now award those soldiers commendations!
+	for (std::map<SoldierRank, Soldier*>::iterator bestSoldier = bestSoldierCandidates.begin(); bestSoldier != bestSoldierCandidates.end(); ++bestSoldier)
+	{
+		(*bestSoldier).second->getDiary()->awardBestOfRank((*bestSoldier).second->getRank());
+		if ((*bestSoldier).second->getDiary()->getScoreTotal() == bestOverallScore)
+		{
+			(*bestSoldier).second->getDiary()->awardBestOverall();
+			bestOverallScore++; // If there is a tie, first come first serve!
+		}
+	}
+
 	for (std::vector<BattleUnit*>::iterator j = battle->getUnits()->begin(); j != battle->getUnits()->end(); ++j)
 	{
 		if ((*j)->getGeoscapeSoldier())
