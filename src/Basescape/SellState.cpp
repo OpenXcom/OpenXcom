ab55d29a92b4334db5ee39e134064a315af03151
Warboy1982 <neo_nihilist@hotmail.com>
2015-07-05 17:55:02 +1000

    get money back for tanks when selling crafts
    these used to be handled as items, guess we changed the implementation
    and forgot to account for this scenario

--- src/Basescape/SellState.cpp
+++ src/Basescape/SellState.cpp
@@ -36,6 +36,7 @@
 #include "../Savegame/Soldier.h"
 #include "../Savegame/Craft.h"
 #include "../Savegame/ItemContainer.h"
+#include "../Savegame/Vehicle.h"
 #include "../Ruleset/RuleItem.h"
 #include "../Ruleset/Armor.h"
 #include "../Ruleset/RuleCraft.h"
@@ -305,6 +306,13 @@ void SellState::btnOkClick(Action *)
 				for (std::map<std::string, int>::iterator it = craft->getItems()->getContents()->begin(); it != craft->getItems()->getContents()->end(); ++it)
 				{
 					_base->getItems()->addItem(it->first, it->second);
+				}
+
+				// Remove vehicles from craft
+				for (std::vector<Vehicle*>::iterator i = craft->getVehicles()->begin(); i != craft->getVehicles()->end(); ++i)
+				{
+					_base->getItems()->addItem((*i)->getRules()->getType());
+					_base->getItems()->addItem((*i)->getRules()->getCompatibleAmmo()->front(), (*i)->getAmmo());
 				}
 
 				// Remove soldiers from craft
