d0cb4d5517b0be7a4728494a589d82b9a4d16b70
Warboy1982 <neo_nihilist@hotmail.com>
2015-07-25 22:21:04 +1000

    no need to center on unit on opening inventory

--- src/Battlescape/InventoryState.cpp
+++ src/Battlescape/InventoryState.cpp
@@ -309,9 +309,6 @@ void InventoryState::init()
 		}
 	}
 
-	if (_parent)
-		_parent->getMap()->getCamera()->centerOnPosition(unit->getPosition(), false);
-
 	unit->setCache(0);
 	_soldier->clear();
 	_btnRank->clear();
