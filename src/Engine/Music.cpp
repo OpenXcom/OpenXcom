bb5c81abcaa9b2d51571fd6a4b28384da50bfb0c
SupSuper <supsuper@gmail.com>
2015-08-17 23:09:34 +0100

    Fix multiple music formats not working correctly

--- src/Engine/Music.cpp
+++ src/Engine/Music.cpp
@@ -91,10 +91,13 @@ void Music::play(int loop) const
 #ifndef __NO_MUSIC
 	if (!Options::mute)
 	{
-		if (_music != 0 && Mix_PlayMusic(_music, loop) == -1)
+		if (_music != 0)
 		{
 			stop();
-			Log(LOG_WARNING) << Mix_GetError();
+			if (Mix_PlayMusic(_music, loop) == -1)
+			{
+				Log(LOG_WARNING) << Mix_GetError();
+			}
 		}
 	}
 #endif
