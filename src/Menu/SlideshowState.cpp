f8628f660a0e8efd3d9d4009bd85989fc84c5aa3
SupSuper <supsuper@gmail.com>
2015-08-29 18:54:02 +0100

    Mod refactoring 1

--- src/Menu/SlideshowState.cpp
+++ src/Menu/SlideshowState.cpp
@@ -112,7 +112,7 @@ void SlideshowState::screenClick(Action *action)
 	++_curScreen;
 
 	// next screen
-	if (_curScreen < _slideshowSlides->size())
+	if (_curScreen < (int)_slideshowSlides->size())
 	{
 		int transitionSeconds = _slideshowHeader.transitionSeconds;
 		if (_slideshowSlides->at(_curScreen).transitionSeconds > 0)
