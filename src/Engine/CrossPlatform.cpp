384e5daef77a8eebed428c65bb85dc88f701bf93
SupSuper <supsuper@gmail.com>
2015-12-26 17:17:27 +0000

    And let's try OSX error dialogs too why not

--- src/Engine/CrossPlatform.cpp
+++ src/Engine/CrossPlatform.cpp
@@ -28,6 +28,9 @@
 #include "Logger.h"
 #include "Exception.h"
 #include "Options.h"
+#ifdef __APPLE__
+#import <Cocoa/Cocoa.h>
+#endif
 #ifdef _WIN32
 #ifndef NOMINMAX
 #define NOMINMAX
@@ -102,6 +105,14 @@ void showError(const std::string &error)
 {
 #ifdef _WIN32
 	MessageBoxA(NULL, error.c_str(), "OpenXcom Error", MB_ICONERROR | MB_OK);
+#elif __APPLE__
+	NSString* msg = [NSString stringWithUTF8String:error.c_str()];
+	NSAlert *alert = [[NSAlert alloc] init];
+	[alert addButtonWithTitle : @"OK"];
+	[alert setMessageText : @"OpenXcom Error"];
+	[alert setInformativeText : msg];
+	[alert setAlertStyle : NSCriticalAlertStyle];
+	[alert performSelectorOnMainThread : @selector(runModal) withObject:nil waitUntilDone : NO];
 #else
 	if (errorDlg.empty())
 	{
