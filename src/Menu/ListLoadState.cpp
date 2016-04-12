3267454a32a6a697f3747aab6756b1041dc65e3a
SupSuper <supsuper@gmail.com>
2016-04-12 21:34:26 +0100

    Compiler fix

--- src/Menu/ListLoadState.cpp
+++ src/Menu/ListLoadState.cpp
@@ -82,7 +82,7 @@ void ListLoadState::lstSavesPress(Action *action)
 		const SaveInfo &saveInfo(_saves[_lstSaves->getSelectedRow()]);
 		for (std::vector<std::string>::const_iterator i = saveInfo.mods.begin(); i != saveInfo.mods.end(); ++i)
 		{
-			if (std::find(Options::mods.begin(), Options::mods.end(), std::pair<std::string, bool>(*i, true)) == Options::mods.end())
+			if (std::find(Options::mods.begin(), Options::mods.end(), std::make_pair(*i, true)) == Options::mods.end())
 			{
 				confirm = true;
 				break;
