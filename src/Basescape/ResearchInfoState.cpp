00fbacde3b52113e175dda6f6d51fe42073c7424
Warboy1982 <neo_nihilist@hotmail.com>
2016-06-27 11:55:23 +1000

    fix research cancel crash

--- src/Basescape/ResearchInfoState.cpp
+++ src/Basescape/ResearchInfoState.cpp
@@ -180,7 +180,7 @@ void ResearchInfoState::btnOkClick(Action *)
 void ResearchInfoState::btnCancelClick(Action *)
 {
 	const RuleResearch *ruleResearch = _rule ? _rule : _project->getRules();
-	if (ruleResearch->needItem() && _rule->destroyItem())
+	if (ruleResearch->needItem() && ruleResearch->destroyItem())
 	{
 		_base->getStorageItems()->addItem(ruleResearch->getName(), 1);
 	}
