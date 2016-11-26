84de73f69ca8d46cfa314fb31e07f599114d812c
SupSuper <supsuper@gmail.com>
2016-11-26 03:24:07 +0000

    Missing include

--- src/fmath.h
+++ src/fmath.h
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <algorithm>
 #include <cfloat>
 #define _USE_MATH_DEFINES
 #include <cmath>
