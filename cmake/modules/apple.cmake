f7bb3d0a1aa74ca5d9399f3ca1a49d812062d0c5
SupSuper <supsuper@gmail.com>
2018-07-23 22:03:00 +0100

    Fix OSX bundle generation (should fix Travis builds)

--- cmake/modules/apple.cmake
+++ cmake/modules/apple.cmake
@@ -1,3 +1,3 @@
 set ( CPACK_BUNDLE_NAME "OpenXcom" )
-set ( CPACK_BUNDLE_ICON "${CMAKE_SOURCE_DIR}/UFO_Enemy_Unknown_opening_screen.png" )
+set ( CPACK_BUNDLE_ICON "${CMAKE_SOURCE_DIR}/res/mac/AppIcon.icns" )
 set ( CPACK_BUNDLE_PLIST "${CMAKE_BINARY_DIR}/openxcom.app/Contents/Info.plist" )
