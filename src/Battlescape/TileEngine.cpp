4dae9d959bf0f0043d7fdf88ccb72ee3b7b1c26c
Meridian <meridian@centrum.sk>
2018-08-05 20:08:13 +0200

    No LOF obstacle indicator vs. empty objects

--- src/Battlescape/TileEngine.cpp
+++ src/Battlescape/TileEngine.cpp
@@ -655,6 +655,7 @@ bool TileEngine::canTargetTile(Position *originVoxel, Tile *tile, int part, Posi
 
 	int minZ = 0, maxZ = 0;
 	bool minZfound = false, maxZfound = false;
+	bool dummy = false;
 
 	if (part == O_OBJECT)
 	{
@@ -716,7 +717,20 @@ bool TileEngine::canTargetTile(Position *originVoxel, Tile *tile, int part, Posi
 		}
 	}
 
-	if (!minZfound) return false;//empty object!!!
+	if (!minZfound)
+	{
+		if (rememberObstacles)
+		{
+			// dummy attempt (only to highlight obstacles)
+			minZfound = true;
+			minZ = 10;
+			dummy = true;
+		}
+		else
+		{
+			return false;//empty object!!!
+		}
+	}
 
 	if (!maxZfound)
 	{
@@ -740,7 +754,20 @@ bool TileEngine::canTargetTile(Position *originVoxel, Tile *tile, int part, Posi
 		}
 	}
 
-	if (!maxZfound) return false;//it's impossible to get there
+	if (!maxZfound)
+	{
+		if (rememberObstacles)
+		{
+			// dummy attempt (only to highlight obstacles)
+			maxZfound = true;
+			maxZ = 10;
+			dummy = true;
+		}
+		else
+		{
+			return false;//it's impossible to get there
+		}
+	}
 
 	if (minZ > maxZ) minZ = maxZ;
 	int rangeZ = maxZ - minZ;
@@ -756,7 +783,7 @@ bool TileEngine::canTargetTile(Position *originVoxel, Tile *tile, int part, Posi
 			scanVoxel->y = targetVoxel.y + spiralArray[i*2+1];
 			_trajectory.clear();
 			int test = calculateLine(*originVoxel, *scanVoxel, false, &_trajectory, excludeUnit, true);
-			if (test == part) //bingo
+			if (test == part && !dummy) //bingo
 			{
 				if (_trajectory.at(0).x/16 == scanVoxel->x/16 &&
 					_trajectory.at(0).y/16 == scanVoxel->y/16 &&
