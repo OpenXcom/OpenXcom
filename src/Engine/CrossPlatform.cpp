e1b3a9b8235c0bfc4a4da76eaa9cef6ecbdd54c2
R1dO <r1denouden@gmail.com>
2019-07-28 02:13:46 +0200

    Only wrap lines when the error itself has a newline.
    Not because the user has a system that thinks 10 characters per line is
    more than enough.

--- src/Engine/CrossPlatform.cpp
+++ src/Engine/CrossPlatform.cpp
@@ -96,7 +96,7 @@ void getErrorDialog()
 		if (getenv("KDE_SESSION_UID") && system("which kdialog 2>&1 > /dev/null") == 0)
 			errorDlg = "kdialog --error ";
 		else if (system("which zenity 2>&1 > /dev/null") == 0)
-			errorDlg = "zenity --error --text=";
+			errorDlg = "zenity --no-wrap --error --text=";
 		else if (system("which kdialog 2>&1 > /dev/null") == 0)
 			errorDlg = "kdialog --error ";
 		else if (system("which gdialog 2>&1 > /dev/null") == 0)
